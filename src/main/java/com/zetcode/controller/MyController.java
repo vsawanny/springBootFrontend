0b72c7babcd435930cbaf27bcb924c49cb0616ef
vsawanny <46169590+vsawanny@users.noreply.github.com>
2020-06-26 20:37:20 +0530

    Update MyController.java

--- src/main/java/com/zetcode/controller/MyController.java
+++ src/main/java/com/zetcode/controller/MyController.java
@@ -1,22 +1,40 @@
 package com.zetcode.controller;
 
 import com.zetcode.bean.User;
+
+import com.microsoft.applicationinsights.TelemetryClient;
+
+ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping; 
 
 @Controller
 public class MyController {
 
+  
+    @Autowired
+     TelemetryClient telemetryClient;
+
+    
+    
     @GetMapping("/addUser")
     public String sendForm(User user) {
+        //track a custom event  
+        telemetryClient.trackEvent("Sending a custom event...");
 
+        //trace a custom trace
+        telemetryClient.trackTrace("Sending a custom trace....");
         return "addUser";
     }
+
 
     @PostMapping("/addUser")
     public String processForm(User user) {
 
         return "showMessage";
     }
+    
+   
 }
